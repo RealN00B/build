33751be71d99f3c9b7f61ba861bab5cb13440f41
Luca Stefani <luca020400@lineageos.org>
2024-10-25 09:05:17 +0000

    build: Add Infinity-X build support
    * Based on "build: Add Lineage build support"
    
    Squashed also with the following changes:
    
      Author: Luca Stefani <luca020400@lineageos.org>
      Date:   Thu Aug 17 22:22:13 2017 +0200
    
        core: Include Lineage pathmap
    
      Change-Id: Ie25dee383cc5bc9bb6390cff2cb2460d526d80b6
    
      Author: Simon Shields <simon@lineageos.org>
      Date:   Fri Mar 2 12:55:47 2018 +1100
    
        build: unconditionally use lineage pathmap
    
        even if we're not building a lineage target, we need to
        use the lineage pathmap to make things like recovery and ril
        happy
    
      Change-Id: I974c30ad10d4ff5b7805c3df9c22010f1e002bf1
    
      Author: Luca Stefani <luca020400@lineageos.org>
      Date:   Fri Aug 18 10:17:00 2017 +0200
    
        envsetup: Call fixup_common_out_dir
    
      Change-Id: I84e16b0df5822cfd8514c46d709109a13c20f86e
    
      Author: Adrian DC <radian.dc@gmail.com>
      Date:   Sat May 12 11:24:33 2018 +0200
    
        build: Include LineageOS specific properties in build.prop
    
         * Our properties were supposed to go to /system/etc/prop.default
            after the following commit:
            "lineage: Move to Google's method of defining system default props"
            Change-Id: I6cb0e28a7599b010b389cc541015a37010a00f4b
    
         * However if BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED is not true,
            only /default.prop will retain the properties contents of
            ADDITIONAL_DEFAULT_PROPERTIES and PRODUCT_SYSTEM_DEFAULT_PROPERTIES,
            and none of our versioning identification was held in the system
    
         * Enabling BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED globally would
            break all properties on devices that handle partitions usually
            at the device level rather than the kernel due to mounting races
    
         * Include the vendor/lineage/config/main.mk entrypoint to be allowed
            to extend ADDITION_BUILD_PROPERTIES as we need to define our releases
    
      Change-Id: I19918eece0f0dd3ee967db0339b4d09c9a6d540e
      Signed-off-by: Adrian DC <radian.dc@gmail.com>
    
      Author: dianlujitao <dianlujitao@lineageos.org>
      Date:   Thu Sep 5 21:56:36 2019 +0800
    
        product: Exclude AOSP apn list on Lineage build
    
        Change-Id: I1fd10089421a2e4b57ef565d87ad1f6036bd1898
    
      Author: Michael Bestas <mkbestas@gmail.com>
      Date:   Fri Dec 4 22:01:21 2020 +0200
    
        Include Calyx sepolicy before including soong config
    
        ...otherwise property_contexts are not generated properly
    
        Change-Id: I426191b8b675b6ab44f79a20e481c732e4efced7
    
    Change-Id: Iab8b2ef8b644a7bea10bfd28b99d9e69539b1357

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -37,5 +37,7 @@ PRODUCT_PACKAGES += \
 
 # Telephony:
 #   Provide a APN configuration to GSI product
+ifeq ($(INFINITY_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif
